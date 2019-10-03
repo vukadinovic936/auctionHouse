16308917d0d403dc04c22aba296e647058e91e11
Milos <mnv180@aubg.edu>
2019-10-03 17:29:51 +0000

    Update ItemsRepository.java

--- src/main/java/com/aubgteam/auctionhouse/Repositories/ItemRepository.java
+++ src/main/java/com/aubgteam/auctionhouse/Repositories/ItemRepository.java
@@ -2,6 +2,6 @@ package com.aubgteam.auctionhouse.Repositories;
 import com.aubgteam.auctionhouse.Models.CreditCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemsRepository extends JpaRepository<CreditCard, Long>{
+public interface ItemRepository extends JpaRepository<CreditCard, Long>{
 
 }
