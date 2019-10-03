ca276cfa86dd18bd278fa6196ef097ebd297ca75
Milos <mnv180@aubg.edu>
2019-10-03 18:05:08 +0000

    Delete ItemRepository.java

--- src/main/java/com/aubgteam/auctionhouse/Repositories/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aubgteam.auctionhouse.Repositories;
-import com.aubgteam.auctionhouse.Models.CreditCard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<CreditCard, Long>{
-
-}
